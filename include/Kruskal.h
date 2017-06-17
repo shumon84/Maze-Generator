3e5a08f9fdc13c0b3401294bc3826ddf951b330b
藤田　朱門 <fujitashumon@shumon.local>
2017-06-17 16:06:51 +0900

    include/* modified

--- include/Kruskal.h
+++ include/Kruskal.h
@@ -10,9 +10,7 @@
 #define KRUSKAL_V ((WIDTH/2)*(DEPTH/2)*HEIGHT) /* 頂点数 */
 #define KRUSKAL_E (KRUSKAL_V*3-(WIDTH/2)*HEIGHT-(DEPTH/2)*HEIGHT-(DEPTH/2)*(WIDTH/2)) /* 辺の数 */
 
-bool kruskal_comp(const Edge& e1,const Edge& e2); /* 辺のコストを比較 */
-Edge* kruskal(Edge *es);		  /* 最小全域木を構築する */
-void kruskal_free(Edge *es);
+Edge* kruskal(Edge *es);	/* 最小全域木を構築する */
 
 #endif
 
--- include/Random.h
+++ include/Random.h
@@ -6,7 +6,7 @@
 #include"Templet.h"
 #include<ctime>
 
-ui random_xorshift();		/* 乱数生成器 */
+ui random_xorshift(void);	/* 乱数生成器 */
 ui random_nm(ui n,ui m);	/* n以上m未満の乱数を生成 */
 
 #endif
